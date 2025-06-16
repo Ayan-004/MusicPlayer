6b83b2691e81b782ef3b5befa71c161f6a9fc55b
Ayan Shaikh <ayyanjsk@gmail.com>
2025-06-16 21:41:04 +0530

    remove unused element

--- music-player/src/components/SearchResults.tsx
+++ music-player/src/components/SearchResults.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useSong } from "./context/SongContext";
-import { motion } from "framer-motion";
 import { AnimatedItem } from "./AnimateItem";
 
 interface Song {
