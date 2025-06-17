6b29ea043c8ce2b842be466e625aa89b24c6c7f2
Ayan Shaikh <ayyanjsk@gmail.com>
2025-06-17 22:13:26 +0530

    solve minor error

--- music-player/src/components/QueuePanel.tsx
+++ music-player/src/components/QueuePanel.tsx
@@ -19,7 +19,7 @@ const QueuePanel = ({ onClose }: Props) => {
         className="fixed right-0 top-0 h-full w-[90%] md:w-[400px] backdrop-blur-2xl xl:backdrop-blur-3xl z-[100] shadow-2xl rounded-l-4xl border-l border-gray-300 p-5 overflow-y-auto"
         >
             <div className="flex justify-between item-center mb-7">
-                <button onClick={clearQueue} className="text-xs bg-white/35 p-3 rounded-full font-montserrat-medium xl:text-4xl hover:cursor-pointer">
+                <button onClick={clearQueue} className="text-xs bg-white/35 p-3 rounded-full font-montserrat-medium hover:cursor-pointer">
                     Clear All
                 </button>
                 <h2 className="text-3xl font-calsans mr-6">Queue</h2>
@@ -54,7 +54,7 @@ const QueuePanel = ({ onClose }: Props) => {
                                 e.stopPropagation();
                                 removeFromQueue(index);
                             }}
-                            className="text-red-500 hover:text-red-700 text-xl"
+                            className="text-red-500 hover:text-red-700 text-xl cursor-pointer"
                             >
                                 <FontAwesomeIcon icon={faTrashCan} />
                             </button>
