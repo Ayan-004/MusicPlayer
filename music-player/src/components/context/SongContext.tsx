7d1f73d7fc10480c435b29c9303488ce5bfdf42f
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-03 20:40:30 +0530

    remove unused useEffect block

--- music-player/src/components/context/SongContext.tsx
+++ music-player/src/components/context/SongContext.tsx
@@ -219,14 +219,14 @@ export const SongProvider = ({ children }: { children: ReactNode }) => {
     }
   }, [currentSong]);
 
-  useEffect(() => {
-    const stored = localStorage.getItem("currentSong");
-    if (stored && !currentSong) {
-      const parsedSong = JSON.parse(stored);
-      setCurrentSong(parsedSong);
-      previousSongUrl.current = parsedSong.url;
-    }
-  }, []);
+  // useEffect(() => {
+  //   const stored = localStorage.getItem("currentSong");
+  //   if (stored && !currentSong) {
+  //     const parsedSong = JSON.parse(stored);
+  //     setCurrentSong(parsedSong);
+  //     previousSongUrl.current = parsedSong.url;
+  //   }
+  // }, []);
 
   return (
     <SongContext.Provider
